7d43330d3fcc715ea55e123d7757a63395921051
Maysam Rabipour <maysam.rabipour@gmail.com>
2020-10-16 04:30:28 +0330

    Finalize the routes & remove additional ones

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,25 @@ Route::middleware('guest')->group(function () {
 });
 
 Route::middleware('auth:api')->group(function () {
+    // Auth Logout & Profile Routes
     Route::post('auth/logout', 'Auth\AuthController@logout')->name('auth.logout');
-    Route::get('auth/profile', 'Auth\AuthController@profile')->name('auth.me');
+    Route::get('auth/profile', 'Auth\AuthController@profile')->name('auth.profile');
+
+    // Roles, Permissions & Users Routes
+    Route::group(['middleware' => ['role:super.admin']], function () {
+        Route::apiResources([
+            'permissions' => 'PermissionController',
+            'roles' => 'RoleController',
+            'users' => 'UserController',
+        ]);
+
+        Route::post('permissions/{permission}/sync-roles', 'PermissionController@syncRoles')->name('sync.roles');
+        Route::post('roles/{role}/sync-permissions', 'RoleController@syncPermissions')->name('sync.permissions');
+        Route::post('users/{user}/sync-roles-and-permissions', 'UserController@syncRolesAndPermissions')->name('sync.roles.permissions');
+    });
+
+    // Tasks Routes
+    Route::apiResources([
+        'tasks' => 'TaskController',
+    ]);
 });
